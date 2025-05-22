1980d8a03b8f97784bc06cd7f044ab80648dbb17
yu-ogi <yusuke_ogiwara@dwango.co.jp>
2025-05-22 17:49:24 +0900

    chore: add akashic.config.mjs

---
+++ akashic.config.mjs
@@ -0,0 +1,10 @@
+/** @type { import("@akashic/akashic-cli-commons").CliConfiguration } */
+export default {
+  commandOptions: {
+    scan: {
+      asset: {
+        usePathAssetId: true,
+      },
+    },
+  },
+};
